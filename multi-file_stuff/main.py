65b1420bd26c991fc5915ca25662b13ffcf35126
Rōdere <bainesharveyjack71@gmail.com>
2026-09-19 05:00:06 +0100

    Delete multi-file_stuff directory
    I realised I could've just used classes, so this was unnecessary

--- multi-file_stuff/main.py
+++
@@ -1,4 +0,0 @@
-import package.player
-from package.player import hp
-p_one = package.player
-p_one.printline(hp)
--- multi-file_stuff/package/player.py
+++
@@ -1,5 +0,0 @@
-inventory = []
-hp = 100
-def printline(hp):
-    print(hp)
-    return hp
