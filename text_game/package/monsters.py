825b73efa49e463c154db46dc1150e7c828f5e79
Rōdere <bainesharveyjack71@gmail.com>
2026-09-21 16:56:01 +0100

    Update monsters.py

--- text_game/package/monsters.py
+++ text_game/package/monsters.py
@@ -1,10 +1,23 @@
 import random
 class Slime_monster():
     def __init__(self):
+        self.name = "Slime monster"
+        self.options = ["Attack", "Defend", "Slime"]
         self.hp = 50
         self.defense = 0.4
         self.dmg = random.randint(30,40)
         self.speed = 3
 
     def damage(self,dmg):
-        self.hp -= dmg - int(dmg*self.defense)
+        tot_dmg = dmg - int(dmg*self.defense)
+        self.hp -= tot_dmg
+        return tot_dmg
+
+class Wendigo():
+    def __init__(self):
+        self.name = "Wendigo"
+        self.options = ["Attack", "Defend", "leave", "Stalk"]
+        self.hp = 70
+        self.defense = 0.3
+        self.dmg = random.randint(45,50)
+        self.speed = 2
